85fb69b9359b49031fdcd124ab05d1bbb2fcb174
niraj6 <nirmal.rajendran@ingka.ikea.com>
2020-05-04 12:15:24 +0200

    Added body-parser to to read data from POST request

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const http = require('http');
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Hello Express 1");
-    next(); // Allows the request to continue to the middleware in line.
+// I get some problem and my terminal showed me below explanation
+
+// body-parser deprecated undefined extended: provide extended option at express
+
+// and i used this app.use(bodyParser.urlencoded({extended: false}))
+
+// or
+
+// you are running a version of Express that is 4.16+ then type just
+
+// app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// app.use((req, res, next) => {
+//     console.log("Hello Express 1");
+//     next(); // Allows the request to continue to the middleware in line.
+// });
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+
 });
 
-app.use((req, res, next) => {
-    console.log("Hello Express 2");
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Header Note</h1>');
 });
 
